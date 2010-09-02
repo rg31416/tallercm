5f482db430d93dd9c4f4ae8710b7764d782e7571
rg3.1416 <rg3.1416@fd4ab7f1-9520-293c-fa75-dc4fd61bd958>
2010-09-02 22:52:30 +0000

    segundo release con nombre y apellido

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo de parte de Rafael");
+		System.out.println("Hola Mundo de parte de Rafael Gonzalez");
 
 	}
 
